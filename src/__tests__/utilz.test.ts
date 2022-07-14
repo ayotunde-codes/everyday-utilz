ff51ae919429890e7bc95ec15a3a171d097f9129
Ayotunde Obasa <ayobam222@gmail.com>
2022-07-14 12:40:24 +0100

    Setup Package

---
+++ src/__tests__/utilz.test.ts
@@ -0,0 +1,90 @@
+import {
+  deleteIndex,
+  isCountry,
+  isEmail,
+  isEmptyString,
+  isLowerCase,
+  isUpperCase,
+  lcFirst,
+  reverseString,
+  stringToArray,
+  ucAllFirst,
+  ucFirst,
+} from '../index';
+
+// 🀄 isEmpty
+test('Is empty string', () => {
+  expect(isEmptyString('Carl')).toBe(false);
+  expect(isEmptyString('')).toBe(true);
+});
+
+// 🀄 isEmail
+test('Is email', () => {
+  expect(isEmail('Carl')).toBe(false);
+  expect(isEmail('you@gmail.com')).toBe(true);
+});
+
+// 🀄 deleteIndex
+test('delete specific index', () => {
+  const array = ['a', 'b', 'c', 'd'];
+  expect(deleteIndex(array, 2)).toStrictEqual(['a', 'b', 'd']);
+});
+
+// 🀄 isCountry
+test('is a country', () => {
+  expect(isCountry('nigeria')).toBe(true);
+  expect(isCountry('not')).toBe(false);
+});
+
+// 🀄 stringToArray
+test('change string to array', () => {
+  expect(stringToArray('nigeria')).toStrictEqual(['n', 'i', 'g', 'e', 'r', 'i', 'a']);
+  expect(isCountry('nigeria')).not.toStrictEqual('nigeria');
+});
+
+// 🀄 isUppercase
+test('check if the entire string is in upper case', () => {
+  expect(isUpperCase('nigeria')).toBe(false);
+  expect(isUpperCase('nigeRIA')).toBe(false);
+  expect(isUpperCase('NIGeRIA')).toBe(false);
+  expect(isUpperCase('NIGERIA')).toBe(true);
+  expect(isUpperCase('NIGERIA IS A COUNTRY')).toBe(true);
+  expect(isUpperCase('NIGERIA IS A COUNTry')).toBe(false);
+  expect(isUpperCase('NIGERIA IS A COuNTry')).toBe(false);
+});
+
+// 🀄 isLowercase
+test('check if the entire string is in upper case', () => {
+  expect(isLowerCase('nigeria')).toBe(true);
+  expect(isLowerCase('nigeRIA')).toBe(false);
+  expect(isLowerCase('NIGeRIA')).toBe(false);
+  expect(isLowerCase('NIGERIA')).toBe(false);
+  expect(isLowerCase('NIGERIA IS A COUNTRY')).toBe(false);
+  expect(isLowerCase('NIGERIA IS A COUNTry')).toBe(false);
+  expect(isLowerCase('nigeria is a country')).toBe(true);
+});
+
+// 🀄 reverseString
+test('reverse a string value', () => {
+  expect(reverseString('nigeria')).toBe('airegin');
+});
+
+// 🀄 lcFirst
+test('make first letter of a string lowercase', () => {
+  expect(lcFirst('Nigeria')).toBe('nigeria');
+  expect(lcFirst('Nigeria')).not.toBe('Nigeria');
+});
+
+// 🀄 ucFirst
+test('make first letter of a string uppercase', () => {
+  expect(ucFirst('Nigeria')).toBe('Nigeria');
+  expect(ucFirst('nigeria')).toBe('Nigeria');
+  expect(ucFirst('nigeria')).not.toBe('nigeria');
+});
+
+// 🀄 ucAllFirst
+test('make first letter  of a word in a string  to uppercase', () => {
+  expect(ucAllFirst('Nigeria we are here')).toBe('Nigeria We Are Here');
+  expect(ucAllFirst('nigeria')).toBe('Nigeria');
+  expect(ucAllFirst('nigeria')).not.toBe('nigeria');
+});
---
+++ src/index.ts
@@ -0,0 +1,283 @@
+// utils 1
+export const isEmptyString = (text: string): boolean => (text ? !`${text}`.trim() : true);
+// util 2
+export const isEmail = (email: string): boolean =>
+  !(/[a-z0-9-.]+@+[a-z0-9-.]+\.+[a-z]{2,}/i.test(email) === false || /[^a-z0-9._@-]/i.test(email) === true);
+
+// util 3
+export const deleteIndex = (array: any[], index: number) => {
+  array.splice(index, 1);
+  return array;
+};
+
+// util 4
+export const countries: string[] = [
+  'afghanistan',
+  'albania',
+  'algeria',
+  'andorra',
+  'angola',
+  'antigua and barbuda',
+  'argentina',
+  'armenia',
+  'australia',
+  'austria',
+  'azerbaijan',
+  'bahamas',
+  'bahrain',
+  'bangladesh',
+  'barbados',
+  'belarus',
+  'belgium',
+  'belize',
+  'benin',
+  'bhutan',
+  'bolivia',
+  'bosnia and herzegovina',
+  'botswana',
+  'brazil',
+  'brunei',
+  'bulgaria',
+  'burkina faso',
+  'burundi',
+  'cote d ivoire',
+  'cabo verde',
+  'cambodia',
+  'cameroon',
+  'canada',
+  'central african republic',
+  'chad',
+  'chile',
+  'china',
+  'colombia',
+  'comoros',
+  'congo',
+  'costa rica',
+  'croatia',
+  'cuba',
+  'cyprus',
+  'czech republic',
+  'democratic republic of the congo',
+  'denmark',
+  'djibouti',
+  'dominica',
+  'dominican republic',
+  'ecuador',
+  'egypt',
+  'el salvador',
+  'equatorial guinea',
+  'eritrea',
+  'estonia',
+  'ethiopia',
+  'fiji',
+  'finland',
+  'france',
+  'gabon',
+  'gambia',
+  'georgia',
+  'germany',
+  'ghana',
+  'greece',
+  'grenada',
+  'guatemala',
+  'guinea',
+  'guineabissau',
+  'guyana',
+  'haiti',
+  'holy see',
+  'honduras',
+  'hungary',
+  'iceland',
+  'india',
+  'indonesia',
+  'iran',
+  'iraq',
+  'ireland',
+  'israel',
+  'italy',
+  'jamaica',
+  'japan',
+  'jordan',
+  'kazakhstan',
+  'kenya',
+  'kiribati',
+  'kuwait',
+  'kyrgyzstan',
+  'laos',
+  'latvia',
+  'lebanon',
+  'lesotho',
+  'liberia',
+  'libya',
+  'liechtenstein',
+  'lithuania',
+  'luxembourg',
+  'macedonia',
+  'madagascar',
+  'malawi',
+  'malaysia',
+  'maldives',
+  'mali',
+  'malta',
+  'marshall islands',
+  'mauritania',
+  'mauritius',
+  'mexico',
+  'micronesia',
+  'moldova',
+  'monaco',
+  'mongolia',
+  'montenegro',
+  'morocco',
+  'mozambique',
+  'myanmar',
+  'namibia',
+  'nauru',
+  'nepal',
+  'netherlands',
+  'new zealand',
+  'nicaragua',
+  'niger',
+  'nigeria',
+  'north korea',
+  'norway',
+  'oman',
+  'pakistan',
+  'palau',
+  'palestine state',
+  'panama',
+  'papua new guinea',
+  'paraguay',
+  'peru',
+  'philippines',
+  'poland',
+  'portugal',
+  'qatar',
+  'romania',
+  'russia',
+  'rwanda',
+  'saint kitts and nevis',
+  'saint lucia',
+  'saint vincent and the grenadines',
+  'samoa',
+  'san marino',
+  'sao tome and principe',
+  'saudi arabia',
+  'senegal',
+  'serbia',
+  'seychelles',
+  'sierra leone',
+  'singapore',
+  'slovakia',
+  'slovenia',
+  'solomon islands',
+  'somalia',
+  'south africa',
+  'south korea',
+  'south sudan',
+  'spain',
+  'sri lanka',
+  'sudan',
+  'suriname',
+  'swaziland',
+  'sweden',
+  'switzerland',
+  'syria',
+  'tajikistan',
+  'tanzania',
+  'thailand',
+  'timorleste',
+  'togo',
+  'tonga',
+  'trinidad and tobago',
+  'tunisia',
+  'turkey',
+  'turkmenistan',
+  'tuvalu',
+  'uganda',
+  'ukraine',
+  'united arab emirates',
+  'united kingdom',
+  'united states of america',
+  'uruguay',
+  'uzbekistan',
+  'vanuatu',
+  'venezuela',
+  'viet nam',
+  'yemen',
+  'zambia',
+  'zimbabwe',
+];
+
+// util 5
+export const isCountry = (country: string): boolean => countries.indexOf(country) !== -1;
+
+// util 5
+export const stringToArray = (stringValue: string): string[] => {
+  const array = [];
+  for (const value of stringValue) {
+    array.push(value);
+  }
+
+  return array;
+};
+
+// util 7
+export const isUpperCase = (text: string): boolean =>
+  !!(text.toUpperCase() !== text.toLowerCase() && text === text.toUpperCase());
+
+// utlil 8
+export const isLowerCase = (text: string): boolean =>
+  !!(text.toUpperCase() !== text.toLowerCase() && text === text.toLowerCase());
+
+// util 9
+
+export const reverseString = (stringValue: string): string => {
+  const arr = stringToArray(`${stringValue}`);
+  return arr.reverse().join('');
+};
+
+// util 10
+export const lcFirst = (value: string): string => {
+  let newValue = '';
+  for (let i = 0; i < value.length; i += 1) {
+    if (i === 0) {
+      newValue = value[0].toLowerCase();
+    } else {
+      newValue += value[i];
+    }
+  }
+  return newValue;
+};
+
+// utils 11
+
+export const ucFirst = (text: string): string => {
+  let newValue = '';
+  for (let i = 0; i < text.length; i += 1) {
+    if (i === 0) {
+      newValue = text[0].toUpperCase();
+    } else {
+      newValue += text[i];
+    }
+  }
+  return newValue;
+};
+
+// util 12
+
+export const ucAllFirst = (word: string, transformAll = true) => {
+  if (!isEmptyString(word)) {
+    const wordArr = word.split(' ');
+    const newWord = wordArr.map((value) => {
+      if (transformAll) {
+        value = value.toLowerCase();
+      }
+      const chars = value.split('');
+      if (chars[0]) chars[0] = chars[0].toUpperCase();
+      return chars.join('');
+    });
+    return newWord.join(' ');
+  }
+  return word;
+};
